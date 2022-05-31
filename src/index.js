dcbb3236508c0e6943a863163c9b8c2b055005af
Ankit Chatterjee <ankitchatterjee@Ankits-MacBook-Pro.local>
2022-05-31 16:47:35 +0530

    source files

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import 'typeface-roboto';
 import registerServiceWorker from './registerServiceWorker';
 import Controller from './screens/Controller';
 
-import ImageListItem from '@material-ui/core/ImageListItem';
-import ImageList from '@material-ui/core/ImageList';
-import ImageListItemBar from '@material-ui/core/ImageListItemBar';
-
 ReactDOM.render(<Controller />, document.getElementById('root'));
 registerServiceWorker();
 
